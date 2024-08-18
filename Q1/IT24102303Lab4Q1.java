1d25d1187af44a422f92b60deaf7de4c9f5ee0b1
IT24102303 <it24102303@my.sliit.lk>
2024-08-18 20:51:10 +0530

    Add files via upload

--- Q1/IT24102303Lab4Q1.java
+++ Q1/IT24102303Lab4Q1.java
@@ -1,16 +1,26 @@
 import java.util.Scanner;
 
 public class IT24102303Lab4Q1 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the number: ");
-        int number = scanner.nextInt();
+    public static void main(String[] args) 
+    {  
+        Scanner input = new Scanner(System.in);
+        int number;
 
-        if (number>0)
-        { System.out.print("The number is : Positive");}
-        else if (number<0)
-        { System.out.print("The number is : Negative");}
+        System.out.print("Enter the number: ");
+        number = input.nextInt();
+
+        String result;
+
+        if (number > 0)
+        { result = "The number is : Positive";}
+
+        else if (number < 0)
+        { result = "The number is : Negative";}
+
         else 
-        {{ System.out.print("The number is : Zero");}}
+        { result = "The number is : Zero";}
+
+        System.out.print(result); 
+
     }
 }
